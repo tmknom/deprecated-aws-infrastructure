42d7ab92abf4b3cf84de4c567268e5e177b0da9b
tmknom <tmk.nom@gmail.com>
2016-01-16 15:16:59 +0900

    rpmリポジトリのレシピ作成

---
+++ configuration/cookbooks/rpm_repository/default.rb
@@ -0,0 +1,22 @@
+# epel
+package 'epel-release'
+
+
+# remi
+package 'http://rpms.famillecollet.com/enterprise/remi-release-6.rpm' do
+  not_if 'rpm -q remi-release'
+end
+
+execute 'disable remi' do
+  command 'sed -i \'s/enabled\s=\s1/enabled=0/g\' /etc/yum.repos.d/remi.repo'
+end
+
+
+# rpmforge
+package 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm' do
+  not_if 'rpm -q rpmforge-release'
+end
+
+execute 'disable rpmforge' do
+  command 'sed -i \'s/enabled\s=\s1/enabled=0/g\' /etc/yum.repos.d/rpmforge.repo'
+end
