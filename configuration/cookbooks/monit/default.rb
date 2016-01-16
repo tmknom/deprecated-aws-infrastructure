ec4a0301927ea2ac2c5646ded239a9e65a0b5318
tmknom <tmk.nom@gmail.com>
2016-01-17 02:21:21 +0900

    監視が止まらないようmonitをupstartに登録

--- configuration/cookbooks/monit/default.rb
+++ configuration/cookbooks/monit/default.rb
@@ -1,3 +1,11 @@
 package 'monit' do
   action :install
 end
+
+remote_file '/etc/init/monit.conf' do
+  # https://github.com/arnaudsj/monit/blob/master/contrib/monit.upstart
+  source 'files/etc/init/monit.conf'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
