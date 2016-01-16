475aff5e5b2f1ee07fae6f68776ba26015f28a89
tmknom <tmk.nom@gmail.com>
2016-01-16 18:47:08 +0900

    ロケール設定をja_JPにするレシピ作成

--- configuration/cookbooks/cloud_init/default.rb
+++ configuration/cookbooks/cloud_init/default.rb
@@ -1,7 +1,16 @@
 CLOUD_INIT_CONFIG = '/etc/cloud/cloud.cfg'
+I18N_CONFIG = '/etc/sysconfig/i18n'
 
 execute 'disable auto update' do
   # http://dev.classmethod.jp/cloud/five-confs-of-ec2-linux-sysops/
   not_if "cat #{CLOUD_INIT_CONFIG} | grep '^repo_upgrade: none'"
   command "sed -i 's/^repo_upgrade:\s\\+security/repo_upgrade: none/' #{CLOUD_INIT_CONFIG}"
+end
+
+execute 'set locale ja_JP' do
+  not_if "cat #{CLOUD_INIT_CONFIG} | grep '^locale: ja_JP.UTF-8'"
+  command <<-EOL
+    echo 'locale: ja_JP.UTF-8' >> #{CLOUD_INIT_CONFIG}
+    sed -i 's/en_US/ja_JP/' #{I18N_CONFIG}
+  EOL
 end
