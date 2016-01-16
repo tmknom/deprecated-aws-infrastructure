a2da0abc57facbcb950c95aa8c1cb2842c596a76
tmknom <tmk.nom@gmail.com>
2016-01-17 02:23:00 +0900

    monitを定期的に再起動するcronを追加

--- configuration/cookbooks/monit/default.rb
+++ configuration/cookbooks/monit/default.rb
@@ -16,3 +16,11 @@ remote_file '/etc/monit.d/sshd.conf' do
   group 'root'
   mode '0600'
 end
+
+execute 'set cron monit' do
+  not_if 'cat /var/spool/cron/root | grep "/usr/bin/monit monitor all"'
+  command <<-EOL
+    echo 'set httpd port 2812 and use address localhost allow localhost' >> /etc/monit.conf
+    echo '0 */1 * * * /usr/bin/monit monitor all' >> /var/spool/cron/root
+  EOL
+end
