aa83069866b947e680eb8020a69736b003206699
tmknom <tmk.nom@gmail.com>
2016-02-01 23:08:48 +0900

    production環境のSubnetを構築するterraform作成

--- orchestration/terraform/module/network/main.tf
+++ orchestration/terraform/module/network/main.tf
@@ -41,3 +41,26 @@ resource "aws_route_table" "private" {
     Environment = "${var.environment}"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  count = "${length(compact(split(",", var.public_subnets)))}"
+  cidr_block = "${element(split(",", var.public_subnets), count.index)}"
+  availability_zone = "${lookup(var.availability_zones, count.index % 2)}"
+  tags {
+    Name = "${var.environment}-public-${count.index}-subnet"
+    Environment = "${var.environment}"
+  }
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  count = "${length(compact(split(",", var.private_subnets)))}"
+  cidr_block = "${element(split(",", var.private_subnets), count.index)}"
+  availability_zone = "${lookup(var.availability_zones, count.index % 2)}"
+  tags {
+    Name = "${var.environment}-private-${count.index}-subnet"
+    Environment = "${var.environment}"
+  }
+}
--- orchestration/vpc/production/main.tf
+++ orchestration/vpc/production/main.tf
@@ -3,4 +3,6 @@ module "production_vpc" {
 
   environment = "production"
   vpc_cidr = "172.17.0.0/16"
+  public_subnets = "172.17.10.0/24,172.17.11.0/24"
+  private_subnets = "172.17.100.0/24,172.17.101.0/24"
 }
