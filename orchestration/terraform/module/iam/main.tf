03b87d4a947c470c8242ac5812e278af5f125672
tmknom <tmk.nom@gmail.com>
2016-02-03 22:43:06 +0900

    IAMグループを構築するterraform作成

--- orchestration/terraform/module/iam/main.tf
+++ orchestration/terraform/module/iam/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_iam_group" "user_group" {
+  name = "${var.role_name}-group"
+  path = "/"
+}
+
 resource "aws_iam_policy" "user_policy" {
   name = "${var.role_name}-policy"
   path = "/"
--- orchestration/terraform/module/iam/outputs.tf
+++ orchestration/terraform/module/iam/outputs.tf
@@ -1,3 +1,11 @@
+output "group_arn" {
+  value = "${aws_iam_group.user_group.arn}"
+}
+
+output "group_name" {
+  value = "${aws_iam_group.user_group.name}"
+}
+
 output "policy_arn" {
   value = "${aws_iam_policy.user_policy.arn}"
 }
