03485f7049c8f0cdbea5037be4a07fbcc9bb2156
tmknom <tmk.nom@gmail.com>
2016-02-03 22:45:15 +0900

    IAMグループとIAMポリシーを紐付けるterraform作成

--- orchestration/terraform/module/iam/main.tf
+++ orchestration/terraform/module/iam/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_iam_policy_attachment" "user_attachment" {
+  name = "${var.role_name}-attachment"
+  policy_arn = "${aws_iam_policy.user_policy.arn}"
+  groups = [
+    "${aws_iam_group.user_group.name}"
+  ]
+}
+
 resource "aws_iam_group" "user_group" {
   name = "${var.role_name}-group"
   path = "/"
