216741c85f7fbdaa24c99f1404967cd3a461dec6
tmknom <tmk.nom@gmail.com>
2016-02-11 14:02:23 +0900

    Fabricをインストールするレシピ作成

---
+++ configuration/cookbooks/fabric/default.rb
@@ -0,0 +1,9 @@
+execute 'install fablic' do
+  not_if "test -e /usr/local/bin/fab"
+  command <<-EOL
+    /usr/local/bin/pip install --upgrade pip
+    /usr/local/bin/pip install fabric
+  EOL
+  user 'root'
+end
+
