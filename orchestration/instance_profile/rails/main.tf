4b2d24559f56348ca1b1fadd59965679de4eec5b
tmknom <tmk.nom@gmail.com>
2016-02-12 12:03:16 +0900

    InstanceProfile定義時にpathを指定するよう変更

--- orchestration/instance_profile/rails/main.tf
+++ orchestration/instance_profile/rails/main.tf
@@ -2,6 +2,7 @@ module "iam" {
   source = "../../terraform/module/instance_profile"
 
   role_name = "rails"
+  path = "/instance_profile/"
   assume_role_policy_json = "assume_role_policy.json"
   policy_json = "policy.json"
 }
--- orchestration/terraform/module/instance_profile/main.tf
+++ orchestration/terraform/module/instance_profile/main.tf
@@ -6,11 +6,13 @@ module "iam_role" {
   source = "../iam_role"
 
   role_name = "${var.role_name}"
+  path = "${var.path}"
   assume_role_policy_json = "${var.assume_role_policy_json}"
   policy_json = "${var.policy_json}"
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "${var.role_name}InstanceProfile"
+  path = "${var.path}"
   roles = ["${module.iam_role.role_name}"]
 }
