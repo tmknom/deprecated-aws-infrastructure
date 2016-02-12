6021c83a97f1789b9b69747847c42b8c840de0b9
tmknom <tmk.nom@gmail.com>
2016-02-12 16:24:29 +0900

    RailsのSecurityGroupを構築するterraform作成

---
+++ orchestration/security_group/rails/main.tf
@@ -0,0 +1,12 @@
+variable "vpc_id" {}        # .bash_profileに環境変数定義 : TF_VAR_vpc_id
+
+module "security_group" {
+  source = "../../terraform/module/security_group"
+
+  name = "Rails"
+  cidr_block = "0.0.0.0/0"
+  vpc_id = "${var.vpc_id}"
+  description = "allow http anywhere"
+
+  port = "3000"
+}
