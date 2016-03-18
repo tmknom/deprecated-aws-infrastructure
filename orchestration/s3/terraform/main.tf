02cd01049c313680a5c6b5a421b76f9f73934721
tmknom <tmk.nom@gmail.com>
2016-03-18 16:05:54 +0900

    terraform用のバケットの定義

---
+++ orchestration/s3/terraform/main.tf
@@ -0,0 +1,14 @@
+variable "s3_suffix" {} # .bash_profileに環境変数定義 : TF_VAR_s3_suffix
+
+variable "identifier" {
+  default = "terraform"
+}
+
+module "s3_administration" {
+  source = "../../terraform/module/s3/administration"
+
+  identifier = "${var.identifier}"
+  suffix = "${var.s3_suffix}"
+
+  policy = "${file("policy.json")}"
+}
