4aa29a986b0d06eae9eec9d0a9973241ce44eb36
tmknom <tmk.nom@gmail.com>
2016-03-19 14:11:05 +0900

    東京リージョンの本番VPC定義

--- orchestration/vpc/production/main.tf
+++ orchestration/vpc/production/main.tf
@@ -2,7 +2,12 @@ module "vpc" {
   source = "../../terraform/module/vpc"
 
   environment = "Production"
-  vpc_cidr = "172.17.0.0/16"
-  public_subnets = "172.17.10.0/24,172.17.11.0/24"
-  private_subnets = "172.17.100.0/24,172.17.101.0/24"
+  region_name = "Tokyo"
+  availability_zones = "ap-northeast-1a,ap-northeast-1c"
+
+  vpc_cidr = "10.10.0.0/16"
+
+  public_subnets = "10.10.0.0/24,10.10.1.0/24"
+  protected_subnets = "10.10.64.0/24,10.10.65.0/24"
+  private_subnets = "10.10.128.0/24,10.10.129.0/24"
 }
