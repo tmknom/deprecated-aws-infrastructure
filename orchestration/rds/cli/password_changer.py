deb729ddb9c9af7189c3d194624eaacdaeb9b843
tmknom <tmk.nom@gmail.com>
2016-03-24 22:25:36 +0900

    RDSのパスワードを設定するヘルパー作成

---
+++ orchestration/rds/cli/password_changer.py
@@ -0,0 +1,24 @@
+# -*- encoding:utf-8 -*-
+
+from fabric.api import *
+
+
+def change_password(db_instance_identifier):
+  db_password = __get_env('DATABASE_PASSWORD')
+  __execute_modify_password(db_instance_identifier, db_password)
+
+
+def __execute_modify_password(db_instance_identifier, db_password):
+  command = ' aws rds modify-db-instance' \
+          + ' --db-instance-identifier %s' % (db_instance_identifier) \
+          + ' --master-user-password "%s"' % (db_password)
+  with hide('running'):
+    local(command)
+
+
+def __get_env(key_name):
+  command = 'echo $%s' % (key_name)
+  with hide('stdout'):
+    result = local(command, capture=True)
+  return result
+
