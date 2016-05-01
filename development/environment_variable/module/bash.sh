b473cc36060250f6367f28f1fa6b42887cf3799e
tmknom <tmk.nom@gmail.com>
2016-05-01 12:39:27 +0900

    作業ログを収集して可視化する「WakaTime」を導入

---
+++ development/environment_variable/module/bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# bash関連
+#
+
+# wakatime
+# http://ktktks.hateblo.jp/entry/2016/04/18/132945
+if [ -e ~/.bash-wakatime/bash-wakatime.sh ]; then
+  source ~/.bash-wakatime/bash-wakatime.sh
+fi
