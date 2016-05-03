5cfa3d79e2319bacbd0d94ae1188cdf520bf4d28
tmknom <tmk.nom@gmail.com>
2016-05-03 13:37:18 +0900

    AMIの作成をFabric経由で実行できるよう修正

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+# -*- encoding:utf-8 -*-
+#
+# 管理用スクリプト
+#
+#####################################################################
+
+from fabric.api import *
+
+BOOTSTRAPPING = 'bootstrapping'
+
+BASE_ROLE = 'base'
+RAILS_ROLE = 'rails'
+
+
+@task
+def ami_base():
+    '''Base AMI の作成'''
+    ami(BASE_ROLE)
+
+
+@task
+def ami_rails():
+    '''Rails AMI の作成'''
+    ami(RAILS_ROLE)
+
+
+def ami(role):
+    with lcd(BOOTSTRAPPING):
+        local('go run bootstrapping.go %s' % (role))
