7ddc475c1f50e43a44d94a56e7e38df446cf56f7
tmknom <tmk.nom@gmail.com>
2016-05-03 13:52:29 +0900

    Administration-Rails-SecurityGroupの定義

--- orchestration/security_group/rails.tf
+++ orchestration/security_group/rails.tf
@@ -9,3 +9,15 @@ module "rails" {
   cidr_block = "0.0.0.0/0"
   vpc_id = "${var.production_vpc_id}"
 }
+
+module "administration_rails" {
+  source = "../terraform/module/security_group/cidr_blocks"
+
+  role = "Rails"
+  description = "allow http only administrator"
+  environment = "${var.administration}"
+
+  port = "80"
+  cidr_block = "${var.localhost_cidr_block}"
+  vpc_id = "${var.administration_vpc_id}"
+}
