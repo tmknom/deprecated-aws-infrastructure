e6139d827ac654b1bbcc925fcc4b318f9b1949b4
tmknom <tmk.nom@gmail.com>
2016-05-03 19:37:51 +0900

    CodeDeployでTestingをAdministrationに変更

--- orchestration/terraform/module/code_deploy/main.tf
+++ orchestration/terraform/module/code_deploy/main.tf
@@ -18,8 +18,8 @@ resource "aws_codedeploy_deployment_group" "production" {
   }
 }
 
-resource "aws_codedeploy_deployment_group" "testing" {
-  deployment_group_name = "testing-${aws_codedeploy_app.application.name}"
+resource "aws_codedeploy_deployment_group" "administration" {
+  deployment_group_name = "administration-${aws_codedeploy_app.application.name}"
 
   app_name = "${aws_codedeploy_app.application.name}"
   service_role_arn = "${var.role_arn}"
@@ -33,7 +33,7 @@ resource "aws_codedeploy_deployment_group" "testing" {
 
   ec2_tag_filter {
     key = "Environment"
-    value = "Testing"
+    value = "Administration"
     type = "KEY_AND_VALUE"
   }
 }
