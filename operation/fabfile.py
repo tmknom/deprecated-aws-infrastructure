f2eb63b6a83d57c3ff7f0be537354fc8ec0749e6
tmknom <tmk.nom@gmail.com>
2016-05-03 13:58:51 +0900

    ssh.pyをsecurity_group.pyにリネーム

--- operation/fabfile.py
+++ operation/fabfile.py
@@ -6,16 +6,16 @@
 
 from fabric.api import *
 
-from security_group import ssh
+from security_group import security_group
 
 
 @task
-def ssh_authorize():
-    '''現在のアドレスからSSH許可'''
-    ssh.authorize()
+def authorize_sg():
+    '''現在のアドレスから管理者VPCへのアクセス許可'''
+    security_group.authorize()
 
 
 @task
-def ssh_revoke():
-    '''SSH許可の取り消し'''
-    ssh.revoke()
+def revoke_sg():
+    '''管理者VPCへのアクセス許可の取り消し'''
+    security_group.revoke()
