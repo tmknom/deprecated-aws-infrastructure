ccecbc93e745eb6b3901129657dd6572a44f411f
tmknom <tmk.nom@gmail.com>
2016-05-05 13:24:05 +0900

    nginxのレシピに設定ファイル関連の記述を追加

--- configuration/cookbooks/nginx/default.rb
+++ configuration/cookbooks/nginx/default.rb
@@ -5,3 +5,17 @@ end
 service 'nginx' do
   action [:enable, :start]
 end
+
+remote_file '/etc/nginx/nginx.conf' do
+  source 'files/etc/nginx/nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
+
+remote_file '/etc/nginx/conf.d/unicorn.conf' do
+  source 'files/etc/nginx/conf.d/unicorn.conf'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
