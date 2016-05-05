a2e1fe6f29ef88a1b5579323ef3ef7830cb96954
tmknom <tmk.nom@gmail.com>
2016-05-05 22:48:17 +0900

    testing用のデプロイグループ作成

--- orchestration/terraform/module/code_deploy/outputs.tf
+++ orchestration/terraform/module/code_deploy/outputs.tf
@@ -3,7 +3,7 @@ output "production_deployment_group_name" {
 }
 
 output "administration_deployment_group_name" {
-  value = "${aws_codedeploy_deployment_group.administration.deployment_group_name}"
+  value = "${aws_codedeploy_deployment_group.testing.deployment_group_name}"
 }
 
 output "application_name" {
