d2af06859d2c0db52cd7f0854a01da14ed03d759
tmknom <tmk.nom@gmail.com>
2016-05-07 16:02:09 +0900

    ユーザ定義レシピで毎回グループを再設定しないようリファクタ

--- configuration/cookbooks/user/default.rb
+++ configuration/cookbooks/user/default.rb
@@ -4,24 +4,25 @@ SSH_SHADOW_PASSWORD = ENV['SSH_SHADOW_PASSWORD']
 SSH_PUBLIC_KEY_FULL_PATH = ENV['SSH_PUBLIC_KEY_FULL_PATH']
 
 # Constant definition
-WHEEL_GROUP = '10'
+WHEEL_GROUP_ID = '10'.to_i
+WHEEL_GROUP_NAME = 'wheel'
 
 # Add user
 user USER_NAME do
   password SSH_SHADOW_PASSWORD
-  gid WHEEL_GROUP
+  gid WHEEL_GROUP_ID
 end
 
 # Setup ssh
 directory "/home/#{USER_NAME}/.ssh" do
   owner USER_NAME
-  group WHEEL_GROUP
+  group WHEEL_GROUP_NAME
   mode '700'
 end
 
 file "/home/#{USER_NAME}/.ssh/authorized_keys" do
   content File.read(SSH_PUBLIC_KEY_FULL_PATH)
   owner USER_NAME
-  group WHEEL_GROUP
+  group WHEEL_GROUP_NAME
   mode '600'
 end
