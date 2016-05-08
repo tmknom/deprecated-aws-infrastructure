4af9cc30958255caa923651dcbb6379daf02936f
tmknom <tmk.nom@gmail.com>
2016-05-08 10:49:32 +0900

    アプリケーション実行用のユーザ作成

--- configuration/roles/rails.rb
+++ configuration/roles/rails.rb
@@ -6,6 +6,8 @@ include_recipe '../cookbooks/nodejs/default.rb'
 include_recipe '../cookbooks/redis/default.rb'
 include_recipe '../cookbooks/ruby/default.rb'
 
+include_recipe '../cookbooks/user/rails.rb'
+
 include_recipe '../cookbooks/nginx/default.rb'
 include_recipe '../cookbooks/logrotate/nginx.rb'
 include_recipe '../cookbooks/monit/nginx.rb'
