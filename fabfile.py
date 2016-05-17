22b21653aa7cf20eda93f1ca392b0fd5a6e0d5ef
tmknom <tmk.nom@gmail.com>
2016-05-17 16:03:07 +0900

    プロジェクト直下以外のタスク一覧を表示するタスク作成

--- fabfile.py
+++ fabfile.py
@@ -8,9 +8,24 @@ from fabric.api import *
 
 from operation.fabfile import prepare
 from operation.fabfile import clear
-from operation.fabfile import sg_authorize
-from operation.fabfile import sg_revoke
 from operation.fabfile import sg_authorize_http
 from operation.fabfile import ec2_list
 from operation.fabfile import ec2_build_testing
 from operation.fabfile import ec2_re_build_testing
+
+
+@task
+def help(module_name=''):
+    '''タスク一覧 [:bootstrapping :configuration :orchestration :operation]'''
+    if module_name == '':
+        fab_list('bootstrapping')
+        fab_list('configuration')
+        fab_list('orchestration')
+        fab_list('operation')
+    else:
+        fab_list(module_name)
+
+
+def fab_list(dir_name):
+    print('\n')
+    local('fab -l -f %s/fabfile.py' % (dir_name))
