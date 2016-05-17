d3460591da15f4e57eba0905277f2b34ce7d6d45
tmknom <tmk.nom@gmail.com>
2016-05-17 15:44:55 +0900

    利用頻度の低いタスクをプロジェクト直下のfabfileから削除

--- fabfile.py
+++ fabfile.py
@@ -6,26 +6,11 @@
 
 from fabric.api import *
 
-from bootstrapping.fabfile import ami_base
-from bootstrapping.fabfile import ami_rails
-from bootstrapping.fabfile import ami_tech_news
-from configuration.fabfile import itamae_base
-from configuration.fabfile import itamae_rails
-from configuration.fabfile import itamae_tech_news
-from configuration.fabfile import spec_base
-from configuration.fabfile import spec_rails
-from configuration.fabfile import spec_tech_news
 from operation.fabfile import prepare
 from operation.fabfile import clear
 from operation.fabfile import sg_authorize
 from operation.fabfile import sg_revoke
 from operation.fabfile import sg_authorize_http
-from operation.fabfile import sg_revoke_http
 from operation.fabfile import ec2_list
 from operation.fabfile import ec2_build_testing
 from operation.fabfile import ec2_re_build_testing
-from operation.fabfile import ec2_start_testing
-from operation.fabfile import ec2_stop_testing
-from operation.fabfile import ec2_remove_testing
-from operation.fabfile import rds_start_administration
-from operation.fabfile import rds_stop_administration
