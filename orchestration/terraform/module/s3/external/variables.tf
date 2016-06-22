e073ed854231dea8d0ae2cc17188d7187ac82372
tmknom <tmk.nom@gmail.com>
2016-06-22 20:28:33 +0900

    externalなS3バケットを作成するモジュールの定義

---
+++ orchestration/terraform/module/s3/external/variables.tf
@@ -0,0 +1,20 @@
+variable "identifier" {
+}
+
+variable "suffix" {
+}
+
+variable "policy" {
+}
+
+variable "group" {
+  default = "External"
+}
+
+variable "acl" {
+  default = "public-read"
+}
+
+variable "environment" {
+  default = "Production"
+}
